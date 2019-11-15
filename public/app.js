0c5b94987265e8d365cbb0e7908a6ce00cdbdf0d
Tamas Biro <tamas.biro@emarsys.com>
2019-11-15 15:35:51 +0100

    toggle label on button

--- public/app.js
+++ public/app.js
@@ -1,10 +1,23 @@
 (function() {
-  var button = document.querySelector('button');
+  var oldMileStonesHidden = true;
   var hiddenElements = document.querySelectorAll('.hidden');
 
-  button.addEventListener('click', function() {
+  var setButtonLabel = function() {
+    var element = document.querySelector('button span');
+    if (oldMileStonesHidden) {
+      element.textContent = 'Show Older Milestones';
+    } else {
+      element.textContent = 'Hide Old Milestones';
+    }
+  };
+
+  setButtonLabel();
+
+  document.querySelector('button').addEventListener('click', function() {
     [].forEach.call(hiddenElements, function(hiddenElement) {
       hiddenElement.classList.toggle('hidden');
     });
+    oldMileStonesHidden = !oldMileStonesHidden;
+    setButtonLabel();
   });
 })();
